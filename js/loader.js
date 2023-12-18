99e79fce7f244c06f51b1979a1f81d3806b2d943
lucky <lucky@no.localhost>
2023-12-17 20:08:07 -0500

    Make the loader good.

--- js/loader.js
+++ js/loader.js
@@ -17,67 +17,55 @@ function firstSuccess(promises) {
       )
     }
   })
-}
-
-command = "";
-function intCmd(command){
-  var commandStart = "";
-  var commandIn = command;
-  commandStart = commandIn.split(" ")[0];
-  if (jsonAppObject.commands[commandStart]){
-    jsonAppObject.commands[commandStart](commandIn);
-  } else {
-    throw new Error("An internal service tried to call the " + commandStart + " command, but it dose not seem to exist.");
-  }
-}
-
-// The list of URLs. The first success will be loaded.
-const URLS = [
+ }
+ 
+function loadScript(urls) {
+  const fetchAborter = new AbortController();
+ 
+  return firstSuccess(
+    urls.map(url =>
+      fetch(url, { signal: fetchAborter.signal })
+        .then(resp => {
+          if (resp.status === 200) {
+            return resp.text();
+          } else {
+            throw new Error(resp.status + " " + resp.statusText);
+          }
+        })
+        .then(content => new Function(content))
+    )
+  )
+    .catch(failures => {
+      throw new Error(`All providers are offline or blocked`);
+    })
+    .then(appye => {
+      fetchAborter.abort();
+      appye();
+    })
+    .catch(err => {
+      alert("Failed to load the script: " + err.message + ".");
+      if(!window.location.hostname == "www.desmos.com"){
+        alert("Try to use the website that is about to open.");
+        open("https://www.desmos.com/testing/virginia/graphing");
+      } else if (window.navigator.onLine == false){
+        alert("Please connect to the internet.");
+      }
+    });
+ }
+ 
+ // The list of URLs. The first success will be loaded.
+ const urlMain = [
   "https://git.basicfan.eu.org/lucky/Appye-Source/raw/branch/main/js/beta.js",
   "https://nmn.pool.net.eu.org/cp/https/git.basicfan.eu.org/lucky/Appye-Source/raw/branch/main/js/beta.js",
   "https://nmn.pool.net.eu.org/cp/64/aHR0cHM6Ly9naXQuYmFzaWNmYW4uZXUub3JnL2x1Y2t5L0FwcHllLVNvdXJjZS9yYXcvYnJhbmNoL21haW4vanMvYmV0YS5qcw==",
-]
+ ];
+ const urlDep = [
+  "https://git.basicfan.eu.org/lucky/Appye-Source/raw/branch/main/dependencies/0.2.6_winbox.bundle.min.js",
+  "https://rawcdn.githack.com/nextapps-de/winbox/0.2.82/dist/winbox.bundle.min.js",
+  "https://raw.githubusercontent.com/nextapps-de/winbox/master/dist/winbox.bundle.min.js"
+ ]
 
-const fetchAborter = new AbortController()
-
-// Take the first successful promise.
-firstSuccess(
-  // For each URL...
-  URLS.map(url =>
-    // Fetch the content.
-    fetch(url, { signal: fetchAborter.signal })
-    .then(resp => {
-      // If there was a success...
-      if (resp.status === 200) {
-        // return the text. (We need to return it since text returns a promise.)
-        return resp.text()
-      } else {
-        // If it failed, throw an error for .catch to handle.
-        throw new Error(resp.status + " " + resp.statusText)
-      }
-    })
-    .then(content => {
-      // Try to parse the javascript. If it succeeds, it can be ran by calling it. If it fails, there is a syntax error.
-      return new Function(content)
-    })
-  )
-)
-.catch(failures => {
-  throw new Error(`All providers are offline or blocked`) // better error message for second .catch
-})
-.then(appye => {
-  // If we're here, a fetch succeeded, so abort the other ones.
-  fetchAborter.abort()
-  // Run the parsed code.
-  //this needs to be changed to a script element.
-  appye()
-})
-.catch(err => {
-  alert("Failed to load the script: " + err.message + ".");
-  if(!window.location.hostname == "www.desmos.com"){
-  alert("Try to use the website that is about to open.");
-  open("https://www.desmos.com/testing/virginia/graphing");
-  } else if (window.navigator.onLine == false){
- alert("Please connect to the internet.");
-  }
-})
+ 
+ loadScript(urlMain);
+ loadScript(urlDep)
+ 
