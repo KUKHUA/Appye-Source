79597224a02bca13963bf2a6413eea3b0a6388da
lucky <lucky@noreply.localhost>
2024-01-09 14:01:02 +0000

    Update 'js/commands.js'

--- js/commands.js
+++ js/commands.js
@@ -224,8 +224,8 @@ appye.createCommand(
     <p>${desc}</p>
     `;
     
-    if (jsonAppObject.apps[apps].internalApp = true){
-    appHtml += `<p>Id: ${apps}</p> <button onclick="intCmd('app-load ${apps}')">Open</button>`
+    if (jsonAppObject.apps[apps].extrnalApp = true){
+    appHtml += `<p>Id: ${apps}</p> <button onclick="window.open('${jsonAppObject.metadata[apps].url}')">Open</button> <p><a href="${jsonAppObject.metadata[apps].url}">or drag this to a new tab</a></p>`
     } else {
       appHtml += `<p>Id: ${apps}</p> <button onclick="intCmd('app-load ${apps}')">Open</button>`
     }
