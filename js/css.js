afc9320e7d4e4f1ce7104aca327152b09cc8cfaf
lucky <lucky@noreply.localhost>
2024-01-12 18:11:29 +0000

    Add 'js/css.js'

---
+++ js/css.js
@@ -0,0 +1,21 @@
+document.head.innerHTML += `
+<style>
+  .wb-h{
+    color: '#249000';
+    font-weight: 800;
+  }
+  
+  .wb-button {
+    font-family: 'Source Code Pro', monospace;
+    font-weight: 500;
+    background-color: '#0F0F0F';
+    color: white;
+  }
+  
+  .wb-body {
+    background-color: '#0F0F0F';
+    color: white;
+    font-family: 'Source Code Pro', monospace;
+  }
+</style>
+`
